33b78932a87abe6d59b64f8107f19e822cdf5adc
samroodAli <devsamrood@gmail.com>
2023-03-17 13:17:02 +0530

    feat: add nextjs debugging in vs code configuration

--- pages/experiments/react-profiler-dev-tools.tsx
+++ pages/experiments/react-profiler-dev-tools.tsx
@@ -2,6 +2,10 @@ export default function Page() {
   return (
     <>
       <p>check the &apos;profiler&apos; section </p>
+      <p>
+        You can click the refresh button in profiler to start recording the
+        initial render
+      </p>
       <SlowComponent />
     </>
   );
