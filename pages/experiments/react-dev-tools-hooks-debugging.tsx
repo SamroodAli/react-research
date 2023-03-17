90ff787951a739c2fefc5e13c2b18d4514f70961
samroodAli <devsamrood@gmail.com>
2023-03-17 12:43:11 +0530

    docs: add documentation on custom hook debugging

--- pages/experiments/react-dev-tools-hooks-debugging.tsx
+++ pages/experiments/react-dev-tools-hooks-debugging.tsx
@@ -4,13 +4,22 @@ export default function Page() {
   // this will show up as CustomHook, without the use keyword
   const [state, setState] = useCustomHook("Initial Value");
 
-  return <div>{state}</div>;
+  return (
+    <>
+      <div>{state}</div>
+      <p>
+        Check the &apos;Component&apos; React dev tools&apos; hooks section
+        under &apos;Page&apos; component
+      </p>
+    </>
+  );
 }
 
 function useCustomHook(initialValue: any = undefined) {
   useDebugValue(
-    //some value we want to show up as the value for the customhook in dev tools
-    "debug Value from useDebugValue"
+    //some value we want to show up as the value for the CustomHook in dev tools
+    // react 'Component' dev tools, under hooks section
+    "debug Value from useDebugValue hook"
   );
 
   return useState(initialValue);
